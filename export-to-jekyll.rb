6d6dde603d7d93bade48f02bfd10143a4d6284ec
Hunter Stevens <onebree@gmail.com>
2016-03-28 14:11:25 -0400

    Add issue number to index

--- export-to-jekyll.rb
+++ export-to-jekyll.rb
@@ -8,13 +8,14 @@ articles = YAML.load_file("article-dump.yaml")
 
 def yaml_frontmatter(article)
   front_matter = {
-    'layout'     => 'post',
-    'title'      => article[:title],
-    'date'       => article[:published].strftime("%F"),
-    'categories' => 'articles',
-    'author'     => author(article),
-    'permalink'  => "articles/#{article[:slug]}",
-    'summary'    => article[:summary]
+    'layout'       => 'post',
+    'title'        => article[:title],
+    'date'         => article[:published].strftime("%F"),
+    'categories'   => 'articles',
+    'author'       => author(article),
+    'permalink'    => "articles/#{article[:slug]}",
+    'summary'      => article[:summary],
+    'issue_number' => article[:issue_number]
   }.to_yaml
   front_matter << "---\n\n"
 end
